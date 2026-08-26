c439a2dd06012c09acf181672403b524873c958e
Syrins <125286268+Saranyapong-K@users.noreply.github.com>
2026-08-26 22:08:01 +0700

    Added more scenes
    added pistol, bullet scene. for later development

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 
-@export var speed = 700.0
+@export var speed = 500.0
 @export var jump_velocity = -200.0
 const airboned_speed_mult = 0.5
-
+	
 func get_input():
 	var input_direction = Input.get_vector("left", "right","up","down")
 	velocity = input_direction * speed
@@ -21,14 +21,23 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("left", "right")
+		
+	if direction != 0:
+		$Sprite2D.flip_h = direction > 0
+		
 	var current_speed = speed
 	
+	#slow when airborned
 	if not is_on_floor():
-		current_speed *= airboned_speed_mult
+		current_speed *= airboned_speed_mult 
 	
 	if direction:
 		velocity.x = direction * current_speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, current_speed)
-
+		
+	if velocity.x != 0 and is_on_floor():
+		$AnimationPlayer.play("walk")
+	else:
+		$AnimationPlayer.stop()
 	move_and_slide()
